a05b473b7d8dba91b8e0fb25a513358f647b0673
Mauricio Dávalos <madvalosj@espe.edu.ec>
2020-07-19 17:44:59 -0500

    baseline v2

--- controllers/destino-controller.js
+++
@@ -1,2 +0,0 @@
-const Sequelize = require('sequelize');
-const Product = require("../models/destino");
--- routes/origen.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const multer = require('multer');
-const checkIfAuthenticated = require('../middlewares/auth-middleware');
-const ProductsController = require('../controllers/origen-controller');
-
